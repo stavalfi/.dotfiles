bdf61ab0b709857aff24d2ce3c0d788b91bea519
stavalfi <stav.alfi@stratoscale.com>
2019-10-28 22:39:31 +0200

    init

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+alias config.fish="vi ~/.config/fish/config.fish"
+alias fishfile="vi ~/.config/fish/fishfile"
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias npmpublic="npm config set registry https://registry.npmjs.org/"
+alias npmprivate="npm config set registry http://npm.dev.wixpress.com"
+alias vimrc='vim ~/.vimrc'
+
+
+set theme_powerline_fonts no
+
+npmprivate
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/Users/stava/.local/share/omf/themes/bobthefish/fish_prompt.fish
